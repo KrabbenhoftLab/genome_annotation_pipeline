fd4a5b2e61de83e1177fabb7d78ce9cfd2867c62
dmacguigan <dmacguig@buffalo.edu>
2023-12-12 12:43:53 -0500

    implemented error handling for unset variables

--- scripts/run_eggNOG.sh
+++ scripts/run_eggNOG.sh
@@ -9,8 +9,8 @@
 
 SPECIES=$1
 ANNOTATION_DIR_CLUSTER=$2
-BRAKER_WEIGHT=$3
-GEMOMA_WEIGHT=$4
+BRAKER_WEIGHT_EGG=$3
+GEMOMA_WEIGHT_EGG=$4
 EGG_THREADS=$5
 EGGNOG_OPTIONS=$6
 
@@ -19,4 +19,4 @@ export EGGNOG_DATA_DIR="/projects/academic/tkrabben/software/eggNOG_db/eggnog-ma
 # need to download new eggNOG DBs?
 # load the modules, export the EGGNOG_DATA_DIR, and run download_eggnog_data.py
 
-emapper.py --cpu ${EGG_THREADS} -i ${SPECIES}_EVM_B${BRAKER_WEIGHT}_G${GEMOMA_WEIGHT}/${SPECIES}.EVM.pep --output_dir ${SPECIES}_EVM_B${BRAKER_WEIGHT}_G${GEMOMA_WEIGHT}_eggNOG -o Engr.EVM.B${BRAKER_WEIGHT}.G${GEMOMA_WEIGHT} --decorate_gff ${SPECIES}_EVM_B${BRAKER_WEIGHT}_G${GEMOMA_WEIGHT}/${SPECIES}.EVM.mod.gff3 --excel ${EGGNOG_OPTIONS}
+emapper.py --cpu ${EGG_THREADS} -i ${SPECIES}_EVM_B${BRAKER_WEIGHT_EGG}_G${GEMOMA_WEIGHT_EGG}/${SPECIES}.EVM.pep --output_dir ${SPECIES}_EVM_B${BRAKER_WEIGHT_EGG}_G${GEMOMA_WEIGHT_EGG}_eggNOG -o Engr.EVM.B${BRAKER_WEIGHT_EGG}.G${GEMOMA_WEIGHT_EGG} --decorate_gff ${SPECIES}_EVM_B${BRAKER_WEIGHT_EGG}_G${GEMOMA_WEIGHT_EGG}/${SPECIES}.EVM.mod.gff3 --excel ${EGGNOG_OPTIONS}
