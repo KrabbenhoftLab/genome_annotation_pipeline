ace5f02572e412c8c8032ad4865a52ab4b24f715
dmacguigan <dmacguig@buffalo.edu>
2024-01-30 14:56:56 -0500

    added checkpointing for RepeatMasker (step 2)

--- scripts/run_repeatMasker_cluster.sh
+++ scripts/run_repeatMasker_cluster.sh
@@ -42,52 +42,65 @@ cd ${ANNOTATION_DIR_CLUSTER}
 mkdir ${SPECIES}_RepeatMasker
 cd ${SPECIES}_RepeatMasker
 
-# check for coding sequences
-# download the uniprot database
-wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
-gunzip uniprot_sprot.fasta.gz
-# filter out transposable element from uniprot database
-# load bbmap module
-module load gcc/11.2.0 openmpi/4.1.1 bbmap/38.98
-filterbyname.sh in=uniprot_sprot.fasta out=uniprot_sprot.TEfiltered.fasta names=transposable,transposase substring=name ignorejunk
 
-# blast against uniprot
-# need to use an older version of BLAST for compatibility with ProtExcluder
-/projects/academic/tkrabben/software/ncbi-blast-2.4.0+/bin/makeblastdb -in uniprot_sprot.TEfiltered.fasta -dbtype prot
-/projects/academic/tkrabben/software/ncbi-blast-2.4.0+/bin/blastx -db uniprot_sprot.TEfiltered.fasta -query ${ANNOTATION_DIR_CLUSTER}/${SPECIES}_RepeatModeler/${REPEAT_LIBRARY_NAME}-families.fa -num_threads ${BLAST_CPUS} -evalue 10 -out ${SPECIES}_blastx_uniprot_TEfiltered_e10.out
+if ! [ -f ./uniprot_sprot.TEfiltered.fasta ]; then
+	# check for coding sequences
+	# download the uniprot database
+	wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+	gunzip uniprot_sprot.fasta.gz
+	# filter out transposable element from uniprot database
+	# load bbmap module
+	module load gcc/11.2.0 openmpi/4.1.1 bbmap/38.98
+	filterbyname.sh in=uniprot_sprot.fasta out=uniprot_sprot.TEfiltered.fasta names=transposable,transposase substring=name ignorejunk
+fi
+
+if ! [ -f ./${SPECIES}_blastx_uniprot_TEfiltered_e10.out ]; then
+	# blast against uniprot
+	# need to use an older version of BLAST for compatibility with ProtExcluder
+	/projects/academic/tkrabben/software/ncbi-blast-2.4.0+/bin/makeblastdb -in uniprot_sprot.TEfiltered.fasta -dbtype prot
+	/projects/academic/tkrabben/software/ncbi-blast-2.4.0+/bin/blastx -db uniprot_sprot.TEfiltered.fasta -query ${ANNOTATION_DIR_CLUSTER}/${SPECIES}_RepeatModeler/${REPEAT_LIBRARY_NAME}-families.fa -num_threads ${BLAST_CPUS} -evalue 10 -out ${SPECIES}_blastx_uniprot_TEfiltered_e10.out
+fi
 
 # load repeatmasker module
 module purge
 module load gcc/11.2.0 openmpi/4.1.1 repeatmodeler/2.0.4.KRAB
 
-# filter out sequences with blast hit, excluding the 50 bp flanking the matched region on each side
-# note that ProtExcluder requires "esl-sfetch" from the Easel library
-# on Bottlerocket, located at "/home/krablab/miniconda2/envs/hmmer/bin/esl-sfetch"
-/projects/academic/tkrabben/software/ProtExcluder/ProtExcluder.pl ${SPECIES}_blastx_uniprot_TEfiltered_e10.out ${ANNOTATION_DIR_CLUSTER}/${SPECIES}_RepeatModeler/${REPEAT_LIBRARY_NAME}-families.fa
+if ! [ -f ${ANNOTATION_DIR_CLUSTER}/${SPECIES}_RepeatModeler/${REPEAT_LIBRARY_NAME}-families.fanoProtFinal ]; then
+	# filter out sequences with blast hit, excluding the 50 bp flanking the matched region on each side
+	# note that ProtExcluder requires "esl-sfetch" from the Easel library
+	# on Bottlerocket, located at "/home/krablab/miniconda2/envs/hmmer/bin/esl-sfetch"
+	/projects/academic/tkrabben/software/ProtExcluder/ProtExcluder.pl ${SPECIES}_blastx_uniprot_TEfiltered_e10.out ${ANNOTATION_DIR_CLUSTER}/${SPECIES}_RepeatModeler/${REPEAT_LIBRARY_NAME}-families.fa
+fi
 
-# Run RepeatMasker to get GFF
-# use coding gene filtered denovo predicted repeats
-mkdir RMask_denovoPrediction_protFiltered
-RepeatMasker -pa ${RM_THREADS} -gff -lib ${ANNOTATION_DIR_CLUSTER}/${SPECIES}_RepeatModeler/${REPEAT_LIBRARY_NAME}-families.fanoProtFinal -dir ./RMask_denovoPrediction_protFiltered ${ANNOTATION_DIR_CLUSTER}/${GENOME_DIR}/${GENOME_FILE}
+if ! [ -f ./RMask_denovoPrediction_protFiltered/*.tbl ]; then
+	# Run RepeatMasker to get GFF
+	# use coding gene filtered denovo predicted repeats
+	mkdir RMask_denovoPrediction_protFiltered
+	RepeatMasker -pa ${RM_THREADS} -gff -lib ${ANNOTATION_DIR_CLUSTER}/${SPECIES}_RepeatModeler/${REPEAT_LIBRARY_NAME}-families.fanoProtFinal -dir ./RMask_denovoPrediction_protFiltered ${ANNOTATION_DIR_CLUSTER}/${GENOME_DIR}/${GENOME_FILE}
+fi
 
-# run RepeatMasker again using a clade or species from the Dfam database
-mkdir RMask_denovoPlusDfam
-RepeatMasker -pa ${RM_THREADS} -gff -species ${RM_SPECIES} -dir ./RMask_denovoPlusDfam ./RMask_denovoPrediction_protFiltered/${GENOME_FILE}.masked
+if ! [ -f ./RMask_denovoPlusDfam/*.tbl ]; then
+	# run RepeatMasker again using a clade or species from the Dfam database
+	mkdir RMask_denovoPlusDfam
+	RepeatMasker -pa ${RM_THREADS} -gff -species ${RM_SPECIES} -dir ./RMask_denovoPlusDfam ./RMask_denovoPrediction_protFiltered/${GENOME_FILE}.masked
+fi
 
-# Combine two rounds of RepeatMasker
-mkdir final_repeat_mask
-gunzip ./RMask_denovoPrediction_protFiltered/*.cat.gz ./RMask_denovoPlusDfam/*.cat.gz
-cat  ./RMask_denovoPrediction_protFiltered/*.cat  ./RMask_denovoPlusDfam/*.cat > final_repeat_mask/${SPECIES}.final_repeat_mask.cat
-gzip ./RMask_denovoPrediction_protFiltered/*.cat
-gzip ./RMask_denovoPlusDfam/*.cat
-cd final_repeat_mask
-ProcessRepeats -species ${RM_SPECIES} ${SPECIES}.final_repeat_mask.cat
+if ! [ -f ./final_repeat_mask/*.tbl ]; then
+	# Combine two rounds of RepeatMasker
+	mkdir final_repeat_mask
+	gunzip ./RMask_denovoPrediction_protFiltered/*.cat.gz ./RMask_denovoPlusDfam/*.cat.gz
+	cat  ./RMask_denovoPrediction_protFiltered/*.cat  ./RMask_denovoPlusDfam/*.cat > final_repeat_mask/${SPECIES}.final_repeat_mask.cat
+	gzip ./RMask_denovoPrediction_protFiltered/*.cat
+	gzip ./RMask_denovoPlusDfam/*.cat
+	cd final_repeat_mask
+	ProcessRepeats -species ${RM_SPECIES} ${SPECIES}.final_repeat_mask.cat
 
-# create GFF
-/projects/academic/tkrabben/modules_KrabLab/easybuild/2023.01/software/avx512/MPI/gcc/11.2.0/openmpi/4.1.1/repeatmasker/4.1.5/util/rmOutToGFF3.pl ${SPECIES}.final_repeat_mask.out > ${SPECIES}.final_repeat_mask.gff3
-# isolate complex repeats
-grep -v -e "Satellite" -e ")n" -e "-rich" ${SPECIES}.final_repeat_mask.gff3 > ${SPECIES}.final_repeat_mask.complex.gff3
-# reformat to work with MAKER
-cat ${SPECIES}.final_repeat_mask.complex.gff3 | \
-  perl -ane '$id; if(!/^\#/){@F = split(/\t/, $_); chomp $F[-1];$id++; $F[-1] .= "\;ID=$id"; $_ = join("\t", @F)."\n"} print $_' \
-  > ${SPECIES}.final_repeat_mask.complex.reformat.gff3
+	# create GFF
+	/projects/academic/tkrabben/modules_KrabLab/easybuild/2023.01/software/avx512/MPI/gcc/11.2.0/openmpi/4.1.1/repeatmasker/4.1.5/util/rmOutToGFF3.pl ${SPECIES}.final_repeat_mask.out > ${SPECIES}.final_repeat_mask.gff3
+	# isolate complex repeats
+	grep -v -e "Satellite" -e ")n" -e "-rich" ${SPECIES}.final_repeat_mask.gff3 > ${SPECIES}.final_repeat_mask.complex.gff3
+	# reformat to work with MAKER
+	cat ${SPECIES}.final_repeat_mask.complex.gff3 | \
+	  perl -ane '$id; if(!/^\#/){@F = split(/\t/, $_); chomp $F[-1];$id++; $F[-1] .= "\;ID=$id"; $_ = join("\t", @F)."\n"} print $_' \
+	  > ${SPECIES}.final_repeat_mask.complex.reformat.gff3
+fi
